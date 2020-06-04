8eafc57e205bb5870167c63c3c5541348a206984
Tomspiano <helloworldbrandnew@163.com>
2020-06-05 00:23:27 +0800

    learning the fundamentals

--- learning/haarcascades/training/globe/cascade_globe.py
+++ learning/haarcascades/training/globe/cascade_globe.py
@@ -1,15 +1,18 @@
 import cv2
 
-pen_cascades = cv2.CascadeClassifier('cascade_pen.xml')
+globe_cascades = cv2.CascadeClassifier('cascade_globe.xml')
 
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    pens = pen_cascades.detectMultiScale(gray, 1.5, 5)
-    for (x, y, w, h) in pens:
+    globes = globe_cascades.detectMultiScale(gray, minNeighbors=30, minSize=(100, 100))
+    for (x, y, w, h) in globes:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        font = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+        cv2.putText(img, 'globe', (x, y - h//5), font, 2, (0, 0, 225), 2)
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
